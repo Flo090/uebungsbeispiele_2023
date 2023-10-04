362a10c9da109b397652e21e543c6728ab597825
Florian@Home <langflorian@msn.com>
2023-10-04 17:32:57 +0200

    Berechnungen Rechteck (Umfang, Flaeche)

---
+++ src/20231004/Rechteck/Rechteck/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechteck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int auswahl;
+
+            Console.WriteLine("Rechteck");
+            Console.Write("Laenge Seite a eingeben: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Laenge Seite b eingeben: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nBitte auswaehlen");
+            Console.WriteLine("================");
+            Console.WriteLine("1 - Flaeche berechnen");
+            Console.WriteLine("2 - Umfang berechnen");
+            
+            Console.Write("\nIhre Wahl: ");
+            auswahl = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            switch (auswahl)
+            {
+                case 1:
+                    int flaeche = a * b;
+                    //Console.Clear();
+                    Console.WriteLine($"Der Flaecheninhalt betraegt {flaeche} m²");
+                    break;
+
+                case 2:
+                    int umfang = (a + b) * 2;
+                    //Console.Clear();
+                    Console.WriteLine($"Der Umfang betraegt {umfang} m");
+                    break;
+
+                default:
+                    Console.WriteLine("Auswahl ungueltig");
+                    break;
+            }
+            //Console.WriteLine($"Seite a: {a}, Seite b:{b}");
+            Console.ReadKey();
+        }
+    }
+}
