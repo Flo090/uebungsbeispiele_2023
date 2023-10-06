777ab03f69bb57fbdcc36d91f5b116126d45940c
Florian@Home <langflorian@msn.com>
2023-10-06 18:29:35 +0200

    Mit try- catch Anweisung ergänzt

--- src/20231006/GeometrischeFiguren/GeometrischeFiguren/Program.cs
+++ src/20231006/GeometrischeFiguren/GeometrischeFiguren/Program.cs
@@ -38,25 +38,24 @@ namespace GeometrischeFiguren
             //  Eingabe je nach Geometrischer Figur...
             //  Anschließend Ausgabe Umfang und Flächeninhalt...
 
-            // Parallelogramm
-            double parallelogrammSeiteA;
-            double parallelogrammSeiteB;
-            double parallelogrammHoeheA;
-            double parallelogrammFlaeche;
-            double parallelogrammUmfang;
-            
-            // Trapez
-            double trapezSeiteA;
-            double trapezSeiteB;
-            double trapezSeiteC;
-            double trapezSeiteD;
-            double trapezHoehe;
-            double trapezFlaeche;
-            double trapezUmfang;
 
+            // Variablen Deklaration & Initialisierung
+            // Parallelogramm
+            double parallelogrammSeiteA = 0.0;
+            double parallelogrammSeiteB = 0.0;
+            double parallelogrammHoeheA = 0.0;
+            double parallelogrammFlaeche = 0.0;
+            double parallelogrammUmfang = 0.0;
+            // Trapez
+            double trapezSeiteA = 0.0;
+            double trapezSeiteB = 0.0;
+            double trapezSeiteC = 0.0;
+            double trapezSeiteD = 0.0;
+            double trapezHoehe = 0.0;
+            double trapezFlaeche = 0.0;
+            double trapezUmfang = 0.0;
             // Auswahl Figur
-            int auswahl;
-            
+            int auswahl = 0;
             // Ueberschriften
             string title = "Geometrische Figuren";
             string ueberschrift = "Uebersicht";
@@ -68,22 +67,38 @@ namespace GeometrischeFiguren
             Console.WriteLine(new string('=', ueberschrift.Length));
             Console.WriteLine("1 - Parallelogramm");
             Console.WriteLine("2 - Trapez");
-
             // Auswahl einlesen
             Console.Write("\nIhre Wahl: ");
-            auswahl = int.Parse(Console.ReadLine());
+            
+            try
+            {
+                auswahl = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n" + ex.Message + "\n");
+                Environment.Exit(1);
+            }
 
             // Abfrage mit if
             if (auswahl == 1)
             {
                 // Parallelogramm
                 // Eingabe
-                Console.Write("\nSeite a: ");
-                parallelogrammSeiteA = double.Parse(Console.ReadLine());
-                Console.Write("Seite b: ");
-                parallelogrammSeiteB = double.Parse(Console.ReadLine());
-                Console.Write("Hoehe ha: ");
-                parallelogrammHoeheA = double.Parse(Console.ReadLine());
+                try
+                {
+                    Console.Write("\nSeite a: ");
+                    parallelogrammSeiteA = double.Parse(Console.ReadLine());
+                    Console.Write("Seite b: ");
+                    parallelogrammSeiteB = double.Parse(Console.ReadLine());
+                    Console.Write("Hoehe ha: ");
+                    parallelogrammHoeheA = double.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message + "\n");
+                    Environment.Exit(1);
+                }
 
                 // Berechnung
                 parallelogrammUmfang = (parallelogrammSeiteA + parallelogrammSeiteB) * 2;
@@ -97,17 +112,24 @@ namespace GeometrischeFiguren
             {
                 // Trapez
                 // Eingabe
-                Console.Write("\nSeite a: ");
-                trapezSeiteA = double.Parse(Console.ReadLine());
-                Console.Write("Seite b: ");
-                trapezSeiteB = double.Parse(Console.ReadLine());
-                Console.Write("Seite c: ");
-                trapezSeiteC = double.Parse(Console.ReadLine());
-                Console.Write("Seite d: ");
-                trapezSeiteD = double.Parse(Console.ReadLine());
-                Console.Write("Hoehe h: ");
-                trapezHoehe = double.Parse(Console.ReadLine());
-
+                try
+                {
+                    Console.Write("\nSeite a: ");
+                    trapezSeiteA = double.Parse(Console.ReadLine());
+                    Console.Write("Seite b: ");
+                    trapezSeiteB = double.Parse(Console.ReadLine());
+                    Console.Write("Seite c: ");
+                    trapezSeiteC = double.Parse(Console.ReadLine());
+                    Console.Write("Seite d: ");
+                    trapezSeiteD = double.Parse(Console.ReadLine());
+                    Console.Write("Hoehe h: ");
+                    trapezHoehe = double.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message + "\n");
+                    Environment.Exit(1);
+                }
                 // Berechnung
                 trapezUmfang = trapezSeiteA + trapezSeiteB + trapezSeiteC + trapezSeiteD;
                 trapezFlaeche = (trapezSeiteA + trapezSeiteC) * trapezHoehe / 2;
