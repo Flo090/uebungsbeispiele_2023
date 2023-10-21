752d092617bc2c7e2d512fd8616c66ba06435e92
Florian@Home <langflorian@msn.com>
2023-10-21 17:16:47 +0200

    Implementierung Json - speichern

---
+++ src/20231021/TeilnehmerVerwaltung_v2/TeilnehmerVerwaltung_v2/TextFileFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeilnehmerVerwaltung_v2
+{
+    internal enum TextFileFormat
+    {
+        Csv,
+        Json,
+        Xml
+    }
+}
