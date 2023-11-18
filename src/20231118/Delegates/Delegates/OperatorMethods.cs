116a7cfb4a9d74bbc3520aad6b28b153503edf3d
Florian@Home <langflorian@msn.com>
2023-11-18 09:33:08 +0100

    Delegates

---
+++ src/20231118/Delegates/Delegates/OperatorMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal abstract class OperatorMethods
+    {
+        public static double Add(double val1, double val2)
+        {
+            return val1 + val2;
+        }
+
+        public static double Modulo(double val1, double val2)
+        {
+            return val1 % val2;
+        }
+    }
+}
---
+++ src/20231118/Delegates/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateHandler calcHandler;
+
+            Console.WriteLine("Grundrechnungsarten");
+
+            calcHandler = new CalculateHandler(OperatorMethods.Add);
+            //calcHandler = OperatorMethods.Modulo;
+            double erg = calcHandler(5, 3);
+
+            Console.WriteLine($"erg={erg}");
+
+            Console.ReadLine();
+        }
+    }
+}
